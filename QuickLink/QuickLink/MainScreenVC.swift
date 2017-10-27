31bc6cb3d09f2339785430d4c2934debdae29036
Faizaan Chishtie <fchishtie@Faizaans-MacBook-Pro.local>
2017-10-26 20:51:38 -0400

    initial

---
+++ QuickLink/QuickLink/MainScreenVC.swift
@@ -0,0 +1,35 @@
+//
+//  MainScreenVC.swift
+//  QuickLink
+//
+//  Created by Faizaan Chishtie on 2017-10-26.
+//  Copyright © 2017 Faizaan Chishtie. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
